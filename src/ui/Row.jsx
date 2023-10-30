cccb2f2cc24edfd48056fa68fcdceecf1dc30812
Catalin Tugui <katamaxx23@gmail.com>
2023-10-31 00:20:11 +0200

    setting up routes

--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -4,20 +4,18 @@ const Row = styled.div`
   display: flex;
 
   ${(props) =>
-    (props.type =
-      "horizontal" &&
-      css`
-        justify-content: space-between;
-        align-items: center;
-      `)}
+    props.type === "horizontal" &&
+    css`
+      justify-content: space-between;
+      align-items: center;
+    `}
 
   ${(props) =>
-    (props.type =
-      "vertical" &&
-      css`
-        flex-direction: column;
-        gap: 1.6rem;
-      `)}
+    props.type === "vertical" &&
+    css`
+      flex-direction: column;
+      gap: 1.6rem;
+    `}
 `;
 
 Row.defaultProps = {
