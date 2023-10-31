737edb92b7e93084f33c2fccfdf8162ef3252c04
Catalin Tugui <katamaxx23@gmail.com>
2023-11-01 00:46:31 +0200

    adding supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://aledbhdodqicjzlgddak.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsZWRiaGRvZHFpY2p6bGdkZGFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg3NjgwODMsImV4cCI6MjAxNDM0NDA4M30.F9R9NnK5UWU4iJYAuYjN4ArbsmZYydCLwLRLftDYwDg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
