dc4fb532765125867aa8c22cedccfa58f745cdf5
Catalin Tugui <katamaxx23@gmail.com>
2023-11-27 13:58:41 +0200

    confirmation delete and modal edit

--- src/features/cabins/useEditCabin.js
+++ src/features/cabins/useEditCabin.js
@@ -3,7 +3,7 @@ import { createEditCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 
 export function useEditCabin() {
-  const queryClient = useQueryClient;
+  const queryClient = useQueryClient();
 
   const { mutate: editCabin, isLoading: isEditing } = useMutation({
     mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
